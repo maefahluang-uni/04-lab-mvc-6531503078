7308d65b60d3b7bd41ea2087cb725115a5df9fa5
SONTHAYA PHUREESAWAT <6531503078@lamduan.mfu.ac.th>
2023-09-11 23:36:27 +0700

    Lab04_6531503078

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -11,7 +11,7 @@ public class Concert {
     private Date date;
     private String location;
     private String description;
-
+    
     public Concert() {
     }
     public Concert(String title, String description) {
@@ -32,6 +32,30 @@ public class Concert {
     //TODO: add getters and setters
     public int getId() {
         return id;
+    }
+    public String getPerformer() {
+        return performer;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public void setDescription(String description) {
+        this.description = description;
     }
     public void setId(int id) {
         this.id = id;
